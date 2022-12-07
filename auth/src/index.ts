4a31b07625bab26f9805ff713678e2026410fe03
Furqan Aziz <furqan.dev6@gmail.com>
2022-12-07 13:01:41 +0500

    backend auth service routing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
 
 const app = express();
 
 app.use(json());
+app.use(currentUserRouter);
+app.use(signupRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
 
 app.get('/api/users/currentuser', (req, res) => {
 	res.send('Hi there!');
