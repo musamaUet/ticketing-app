a935967dbca45de3d96b395306eaab4f579913f4
musamaUet <usamaitians.gcuf@gmail.com>
2022-12-21 22:14:11 +0500

    setting up test cases and write first testCase with jest

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,29 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+let mongo: any;
+
+beforeAll(async () => {
+	process.env.JWT_KEY = 'asdfasdf';
+
+	mongo = new MongoMemoryServer();
+	await mongo.start();
+	const mongoUri = await mongo.getUri();
+
+	mongoose.set('strictQuery', false);
+	await mongoose.connect(mongoUri);
+});
+
+beforeEach(async () => {
+	const collections = await mongoose.connection.db.collections();
+
+	for (let colleciton of collections) {
+		await colleciton.deleteMany({});
+	}
+});
+
+afterAll(async () => {
+	await mongo.stop();
+	await mongoose.connection.close();
+});
